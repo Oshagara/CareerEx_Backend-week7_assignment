68959584bf3c2186536bf38d8fd8225ca33c87bd
Israel Oluwapelumi Oshagara <oshagaras@gmail.com>
2025-05-07 10:05:10 +0100

    Optimization

--- lost-found-system/server.js
+++ lost-found-system/server.js
@@ -11,6 +11,14 @@ app.use(express.json());
 
 app.use('/api/items', itemRoutes);
 
+// Catch JSON syntax errors
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+      return res.status(400).json({ error: 'Invalid JSON format in request body' });
+    }
+    next();
+  });
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
